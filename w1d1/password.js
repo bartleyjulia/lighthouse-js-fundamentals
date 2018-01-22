8a2e003b730c495c97291e2194ad91a3f90f6641
Julia Bartley <bartleyjulia@gmail.com>
2018-01-22 00:52:23 +0000

    MusicLibrary

---
+++ w1d1/password.js
@@ -0,0 +1,38 @@
+var password = process.argv.slice(2);
+function obfuscate(password) {
+  switch (password[0]) {
+    case 'a':
+    console.log(howdedoodee);
+    break;
+    }
+}
+console.log(obfuscate(password[0]));
+
+
+
+
+
+
+
+
+//var password = process.argv.slice(2);
+// function obfuscate(password) {
+  // var regex = /[a]/g;
+  // var regex1 = /[e]/g;
+  // var regex2 = /[o]/g;
+  // var regex3 = /[l]/g;
+  // if (password.match(regex)) {
+    // password = password.replace(regex, "4")
+  // }
+  // if (password.match(regex1)) {
+    // password = password.replace(regex1, "3")
+  // }
+  // if (password.match(regex2)) {
+    // password = password.replace(regex2, "0")
+  // }
+  // if (password.match(regex3)) {
+    // password = password.replace(regex3, "1")
+  // }
+  // return password;
+// }
+// console.log(obfuscate(password[0]));
